61371e2136a539b7809650f83b6b833f7382ee18
Peter Stackebrandt <peter.stackebrandt@googlemail.com>
2022-01-25 20:13:17 +0100

    Finished project zookeeper

--- Zookeeper/task/zookeeper/zookeeper.py
+++ Zookeeper/task/zookeeper/zookeeper.py
@@ -119,7 +119,19 @@ Switching on the camera in the rabbit habitat...
 (" ~----( ~   Y.  )
 It looks like we will soon have more rabbits!"""
 
-
 animals = [camel, lion, deer, goose, bat, rabbit]
 
 # write your code here
+EXIT = "exit"
+
+myInput = ""
+while True:
+    print("Please enter the number of the habitat you would like to view:")
+    myInput = input()
+    myInput = myInput.lower()
+    if myInput == EXIT:
+        break
+    if myInput.isdigit():
+        print(animals[int(myInput)])
+
+print("See you later!")
